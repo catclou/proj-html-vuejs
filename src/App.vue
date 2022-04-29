6c4bd51b3113c27bff331f195b681dc2dae37a81
catclou <caterina.moccia@gmail.com>
2022-04-29 16:56:27 +0200

    creato progetto

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1 class="text-success">Titolo di prova <i class="fa-solid fa-circle-check"></i></h1>
+  </div>
+</template>
+
+<script>
+import "bootstrap"
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+@import "bootstrap/dist/css/bootstrap.min.css";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
