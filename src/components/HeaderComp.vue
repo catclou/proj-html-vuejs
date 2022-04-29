f2d0a686d88a7162bda14ea719bca913ded53031
catclou <caterina.moccia@gmail.com>
2022-04-29 20:36:02 +0200

    completato HeroFirst

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,17 +1,22 @@
 <template>
     <header class="d-flex justify-content-between">
-        <img src="../assets/img/logo_seo_1x.png" alt="">
+
+        <!-- logo -->
+        <img src="../assets/img/logo_seo_w_1x.png" alt="">
+
+        <!-- navbar -->
         <nav class="h-100">
             <ul class="list-unstyled d-flex h-100">
                 <a href="#" id="home" class="ms-3 text-capitalize list-group-item">home</a>
-                <a href="#" class="ms-3 text-capitalize list-group-item">who we are</a>
-                <a href="#" class="ms-3 text-capitalize list-group-item">what we do</a>
-                <a href="#" class="ms-3 text-capitalize list-group-item">Where We Work</a>
-                <a href="#" class="ms-3 text-capitalize list-group-item">Careers <button class="text-uppercase btn btn-outline-warning btn-sm rounded ms-1">apply</button></a>
-                <a href="#" class="ms-3 text-capitalize list-group-item">news</a>
+                <a href="#" class="ms-3 text-capitalize list-group-item text-white">who we are</a>
+                <a href="#" class="ms-3 text-capitalize list-group-item text-white">what we do</a>
+                <a href="#" class="ms-3 text-capitalize list-group-item text-white">Where We Work</a>
+                <a href="#" class="ms-3 text-capitalize list-group-item text-white">Careers <button class="text-uppercase btn btn-outline-warning btn-sm rounded ms-1">apply</button></a>
+                <a href="#" class="ms-3 text-capitalize list-group-item text-white">news</a>
                 <button id="get_in_touch" class="btn btn-warning btn-sm rounded-pill ms-3 text-uppercase text-white">get in touch now</button>
             </ul>
         </nav>
+
     </header>
 </template>
 
@@ -24,7 +29,7 @@ export default {
 
 <style scoped lang="scss">
     header {
-        height: 10vh;
+        height: 5vh;
         width: 70%;
         align-self: center;
         margin: 10px;
@@ -32,10 +37,12 @@ export default {
 
         img {
             height: 40px;
+            z-index: 1;
         }
 
         .list-group-item {
             border: none!important;
+            background-color: transparent;
         }
 
         a:hover, #home {
@@ -49,6 +56,7 @@ export default {
             font-size: 0.8rem;
             font-weight: 600;
             background-color: #ffa837!important;
+            z-index: 1;
         }
 
         .btn-group-sm > .btn, .btn-sm {
--- src/components/HeroFirst.vue
+++ src/components/HeroFirst.vue
@@ -1,26 +1,71 @@
 <template>
-    <div>
+    <!-- hero -->
+    <div id="hero" class="container-fluid d-flex align-middle">
 
+        <!-- text and button container -->
+        <div class="row d-flex w-100">
+            <div class="col-4 align_middle m_custom d-flex flex-column justify-content-between">
+
+                <!-- title -->
+                <h1 class="w-100 text-light text-start">SEM Campaigns Made Simple With Avada</h1>
+
+                <!-- paragraph -->
+                <p class="text-start text-light fs-5 mt-4 mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu ornare erat. Curabitur pulvinar elit.</p>
+                
+                <!-- buttons -->
+                <div class="d-flex">
+                    <button id="buy" class="btn btn-sm rounded-pill text-uppercase text-white">buy avada now</button>
+                    <button id="contact" class="btn btn-sm rounded-pill ms-3 text-uppercase text-white">contact us</button>
+                </div>
+            
+            </div>
+        </div>
+        
     </div>
 </template>
 
 <script>
-// import "bootstrap"
-// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-    name: 'HeroFirst',
-    components: {
-        // HelloWorld
-    }
+    name: 'HeroFirst'
 }
 </script>
 
 <style scoped lang="scss">
-// @import "bootstrap/dist/css/bootstrap.min.css";
-div {
-        height: 10vh;
-        width: 100%;
-        border: 2px solid salmon;
+
+#hero {
+        height: 75vh;
+        margin-top: -7.2vh;
+        background-image: url('../assets/img/1-hero-image.png');
+        background-size: cover;
+
+        .align_middle {
+            align-self: center;
+        }
+
+        .m_custom {
+            margin-left: 18vw;
+        }
+
+        h1 {
+            font-size: 3rem;
+            font-weight: bold;
+        }
+
+        button {
+            width: 150px;
+            height: 40px;
+            padding: 4px!important;
+            font-size: 0.8rem;
+            font-weight: 600;
+        }
+
+        #buy {
+            background-color: #ffa837!important;
+        }
+
+        #contact {
+            background-color: #556eed!important;
+        }
     }
 </style>
